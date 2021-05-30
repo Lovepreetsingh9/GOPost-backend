55541e69787c04ba6e116827355e89bfc972a7c3
Love <love9991141037@gmail.com>
2021-05-30 23:30:00 +0530

    setting mongoose

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://Lovepreet:<password>@cluster0.fnl8y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
-
+// mongoose.connect('mongodb+srv://Lovepreet:1NYrJxeUgBqNEUSg@cluster0.fnl8y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+mongoose.connect("mongodb+srv://Lovepreet:1NYrJxeUgBqNEUSg@cluster0.fnl8y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
