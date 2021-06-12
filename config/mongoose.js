8304aa83a0c80fcd80a04202ed2abd82b2a52f0a
Love <love9991141037@gmail.com>
2021-06-12 10:15:20 +0530

    update

--- config/mongoose.js
+++ config/mongoose.js
@@ -11,7 +11,7 @@ db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
 
 
 db.once('open', function(){
-    // console.log('Connected to Database :: MongoDB');
+    console.log('Connected to Database :: MongoDB');
 });
 
 
